1232833ea455a91df2bd15d27243eca9fe50b846
harbor <85098287@qq.com>
2019-05-19 19:04:10 +0800

    add test script

---
+++ src/test/java/com/example/demo/TestJasypt.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Created by harbor on 2019/5/19.
+ */
+public class TestJasypt {
+
+//    @Autowired
+//    StringEncryptor stringEncryptor;
+//
+//    @Test
+//    public void encryptPwd() {
+//        String result = stringEncryptor.encrypt("yourpassword");
+//        System.out.println("==================");
+//        System.out.println(result);
+//        System.out.println("==================");
+//    }
+
+
+}
